32a1b5ed6147a58fc79183199b7941ab56e0aa32
Raquel Reis <raquelreis233@gmail.com>
2024-05-26 08:04:44 -0300

    task format

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -10,8 +10,9 @@ from app.pipeline.load import load_excel
 from app.pipeline.transform import concat_data_frames
 
 # Sample data for testing
-df1 = pd.DataFrame({"A": [1, 2, 3], "B": ["a", "b", "c"]})
-df2 = pd.DataFrame({"A": [4, 5, 6], "B": ["d", "e", "f"]})
+df1 = pd.DataFrame({'A': [1, 2, 3], 'B': ['a', 'b', 'c']})
+df2 = pd.DataFrame({'A': [4, 5, 6], 'B': ['d', 'e', 'f']})
+
 
 @pytest.fixture
 def mock_input_folder(tmpdir):
@@ -19,19 +20,17 @@ def mock_input_folder(tmpdir):
     Fixture to create mock input folder with sample Excel files for testing.
     """
     # Creating sample Excel files for testing
-    input_folder = tmpdir.mkdir("input_folder")
-    df1.to_excel(
-        input_folder.join("file1.xlsx"), index=False
-    )  
-    df2.to_excel(
-        input_folder.join("file2.xlsx"), index=False
-    )  
+    input_folder = tmpdir.mkdir('input_folder')
+    df1.to_excel(input_folder.join('file1.xlsx'), index=False)
+    df2.to_excel(input_folder.join('file2.xlsx'), index=False)
     return str(input_folder)
+
 
 @pytest.fixture
 def mock_output_folder(tmpdir):
     """Fixture to create a mock output folder for testing."""
-    return str(tmpdir.mkdir("output_folder"))
+    return str(tmpdir.mkdir('output_folder'))
+
 
 def test_extract(mock_input_folder):
     """Test the extraction of data from the input folder."""
@@ -39,47 +38,50 @@ def test_extract(mock_input_folder):
     assert len(extracted_data) == 2  # Expecting two DataFrames
     assert all(isinstance(df, pd.DataFrame) for df in extracted_data)
 
+
 def test_extract_no_files(tmpdir):
     """Test extraction functionality with an empty input folder."""
     # Creating empty folder
-    empty_folder = tmpdir.mkdir("empty_folder")
-    with pytest.raises(ValueError, match="No Excel files found"):
-        extract_from_excel(str(empty_folder))   
-        
+    empty_folder = tmpdir.mkdir('empty_folder')
+    with pytest.raises(ValueError, match='No Excel files found'):
+        extract_from_excel(str(empty_folder))
+
+
 def test_transform():
     """Test the transformation of dataframes."""
     data = [df1, df2]
     consolidated_df = concat_data_frames(data)
     assert len(consolidated_df) == 6  # 3 rows from df1 + 3 rows from df2
-    assert list(consolidated_df.columns) == ["A", "B"]
-    
+    assert list(consolidated_df.columns) == ['A', 'B']
+
+
 def test_transform_empty_list():
     """Test the transformation functionality with an empty list."""
     empty_list = []
-    with pytest.raises(ValueError, match="No data to transform"):
+    with pytest.raises(ValueError, match='No data to transform'):
         concat_data_frames(empty_list)
-        
+
+
 def test_load_no_permission(tmpdir):
     """Test the load functionality with a protected output folder."""
     # Assuming the folder doesn't have write permissions
-    protected_folder = tmpdir.mkdir("protected_folder")
+    protected_folder = tmpdir.mkdir('protected_folder')
     os.chmod(str(protected_folder), 0o444)  # Read-only permissions
 
-    df = pd.DataFrame({"A": [1], "B": ["a"]})
+    df = pd.DataFrame({'A': [1], 'B': ['a']})
     with pytest.raises(PermissionError):
-        load_excel(df, str(protected_folder), "test.xlsx")
-        
+        load_excel(df, str(protected_folder), 'test.xlsx')
+
+
 def test_load(mock_output_folder):
     """Test the load functionality."""
     df = pd.concat([df1, df2], axis=0, ignore_index=True)
-    output_file_name = "consolidated.xlsx"
+    output_file_name = 'consolidated.xlsx'
     load_excel(df, mock_output_folder, output_file_name)
     assert os.path.exists(os.path.join(mock_output_folder, output_file_name))
 
     # Verifying the contents of the loaded file
     loaded_df = pd.read_excel(
         os.path.join(mock_output_folder, output_file_name)
-    )  
+    )
     pd.testing.assert_frame_equal(loaded_df, df)
-    
-    
